65db444013a19935b9fb096024716f6fa8d89fee
dsdsddd1 <dsdsdddw@gmail.com>
2026-06-04 15:43:53 +0800

    feat: add right-click context menu to close application

--- SystemMonitor/SystemMonitor/MainWindow.xaml.cs
+++ SystemMonitor/SystemMonitor/MainWindow.xaml.cs
@@ -60,6 +60,11 @@ public partial class MainWindow : Window
         }
     }
 
+    private void CloseMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
     protected override void OnClosed(EventArgs e)
     {
         _timer.Stop();
