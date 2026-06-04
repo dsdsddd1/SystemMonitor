e02cb99b46a1efa2ecf8667b3803cfd985c9e9fa
dsdsddd1 <dsdsdddw@gmail.com>
2026-06-04 15:23:05 +0800

    feat: configure app startup with single instance check

--- SystemMonitor/SystemMonitor/App.xaml.cs
+++ SystemMonitor/SystemMonitor/App.xaml.cs
@@ -1,12 +1,27 @@
-using System.Configuration;
-using System.Data;
 using System.Windows;
 
 namespace SystemMonitor;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        // Ensure single instance
+        var currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+        var processes = System.Diagnostics.Process.GetProcessesByName(currentProcess.ProcessName);
+        
+        if (processes.Length > 1)
+        {
+            MessageBox.Show("程序已在运行中", "系统监控", MessageBoxButton.OK, MessageBoxImage.Information);
+            Current.Shutdown();
+            return;
+        }
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+    }
 }
