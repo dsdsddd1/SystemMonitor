e01f00e8cbafe4e0bf68b479f8918d92ec2393c7
dsdsddd1 <dsdsdddw@gmail.com>
2026-06-04 15:14:24 +0800

    feat: implement HardwareMonitor service

---
+++ SystemMonitor/SystemMonitor/HardwareMonitor.cs
@@ -0,0 +1,128 @@
+using LibreHardwareMonitor.Hardware;
+using SystemMonitor.Models;
+
+namespace SystemMonitor;
+
+public class HardwareMonitor : IDisposable
+{
+    private Computer? _computer;
+    private bool _disposed;
+
+    public void Initialize()
+    {
+        _computer = new Computer
+        {
+            IsCpuEnabled = true,
+            IsGpuEnabled = true,
+            IsMemoryEnabled = true
+        };
+        _computer.Open();
+    }
+
+    public SystemInfo GetSystemInfo()
+    {
+        var info = new SystemInfo();
+        
+        if (_computer == null) return info;
+        
+        foreach (var hardware in _computer.Hardware)
+        {
+            hardware.Update();
+            
+            foreach (var sensor in hardware.Sensors)
+            {
+                ProcessSensor(hardware, sensor, info);
+            }
+            
+            foreach (var subHardware in hardware.SubHardware)
+            {
+                subHardware.Update();
+                foreach (var sensor in subHardware.Sensors)
+                {
+                    ProcessSensor(subHardware, sensor, info);
+                }
+            }
+        }
+        
+        return info;
+    }
+
+    private void ProcessSensor(IHardware hardware, ISensor sensor, SystemInfo info)
+    {
+        if (!sensor.Value.HasValue) return;
+        
+        float value = sensor.Value.Value;
+        
+        switch (hardware.HardwareType)
+        {
+            case HardwareType.Cpu:
+                ProcessCpuSensor(sensor, value, info);
+                break;
+            case HardwareType.GpuNvidia:
+            case HardwareType.GpuAmd:
+            case HardwareType.GpuIntel:
+                ProcessGpuSensor(sensor, value, info);
+                break;
+            case HardwareType.Memory:
+                ProcessMemorySensor(sensor, value, info);
+                break;
+        }
+    }
+
+    private void ProcessCpuSensor(ISensor sensor, float value, SystemInfo info)
+    {
+        switch (sensor.SensorType)
+        {
+            case SensorType.Load when sensor.Name.Contains("CPU Total"):
+                info.CpuUsage = value;
+                break;
+            case SensorType.Temperature when sensor.Name.Contains("CPU Package"):
+                info.CpuTemperature = value;
+                break;
+        }
+    }
+
+    private void ProcessGpuSensor(ISensor sensor, float value, SystemInfo info)
+    {
+        switch (sensor.SensorType)
+        {
+            case SensorType.Load when sensor.Name.Contains("GPU Core"):
+                info.GpuUsage = value;
+                break;
+            case SensorType.Temperature when sensor.Name.Contains("GPU Core"):
+                info.GpuTemperature = value;
+                break;
+            case SensorType.SmallData when sensor.Name.Contains("GPU Memory Used"):
+                info.GpuMemoryUsed = value;
+                break;
+            case SensorType.SmallData when sensor.Name.Contains("GPU Memory Total"):
+                info.GpuMemoryTotal = value;
+                break;
+        }
+    }
+
+    private void ProcessMemorySensor(ISensor sensor, float value, SystemInfo info)
+    {
+        switch (sensor.SensorType)
+        {
+            case SensorType.Load when sensor.Name.Contains("Memory"):
+                info.MemoryUsed = value;
+                break;
+            case SensorType.Data when sensor.Name.Contains("Used Memory"):
+                info.MemoryUsed = value * 1024;
+                break;
+            case SensorType.Data when sensor.Name.Contains("Available Memory"):
+                info.MemoryTotal = (info.MemoryUsed / 1024 + value) * 1024;
+                break;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            _computer?.Close();
+            _disposed = true;
+        }
+    }
+}
